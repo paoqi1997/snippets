6f4810275de396d4a540be9d09c8ae59182b886e
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2018-06-22 01:09:42 +0800

    Lua代码段

---
+++ lua/UNIT-01.lua
@@ -0,0 +1,37 @@
+--[[
+    Version: 5.3.4
+
+    Theme: Lua之基本类型
+--]]
+
+-- 剩下一种基本类型 userdata 并未给出
+
+_nil = nil
+
+print(type(_nil))       -- nil
+
+_boolean = true
+
+print(type(_boolean))   -- boolean
+
+_number = 1024
+
+print(type(_number))    -- number
+
+_string = 'Hello World!'
+
+print(type(_string))    -- string
+
+function _function(name)
+    return name .. ' ' .. 'loves me.'
+end
+
+print(type(_function))  -- function
+
+_coroutine = coroutine.create(_function)
+
+print(type(_coroutine)) -- thread
+
+_table = {'Apache', 'IIS', 'Nginx'}
+
+print(type(_table))     -- table
