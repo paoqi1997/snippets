5b0b58d5225f08d632652ad7fc616e6b5275604e
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2018-07-16 17:28:35 +0800

    Lua代码段

---
+++ lua/UNIT-07/dev.lua
@@ -0,0 +1,10 @@
+--[[
+    Version: 5.3.4
+    Theme: Lua之模块
+--]]
+
+local m = require('mod')
+
+m.broadcast()      -- Show me the code!
+
+module.broadcast() -- Show me the code!
---
+++ lua/UNIT-07/mod.lua
@@ -0,0 +1,12 @@
+--[[
+    Version: 5.3.4
+    Theme: Lua之模块
+--]]
+
+module = {}
+
+function module.broadcast()
+    print('Show me the code!')
+end
+
+return module
