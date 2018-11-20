76dbe2c504b61c98c3c78fb152f5fb02f91bafe2
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2018-11-20 15:59:41 +0800

    Update File

--- shell/UNIT-02.sh
+++ shell/UNIT-02.sh
@@ -8,35 +8,35 @@ dll='/usr/local/lib/libpao.so.1'
 
 echo $dll        # /usr/local/lib/libpao.so.1
 
-#
+# 删除前4个字符
 echo ${dll#????} # /local/lib/libpao.so.1
 
-#
+# 从左边开始删除字符，删除第一个'/'后结束
 echo ${dll#*/}   # usr/local/lib/libpao.so.1
-#
+# 从左边开始删除字符，删除最后一个'/'后结束
 echo ${dll##*/}  # libpao.so.1
-#
+# 从左边开始删除字符，删除第一个'.'后结束
 echo ${dll#*.}   # so.1
-#
+# 从左边开始删除字符，删除最后一个'.'后结束
 echo ${dll##*.}  # 1
 
-#
+# 从右边开始删除字符，删除第一个'/'后结束
 echo ${dll%/*}   # /usr/local/lib
-#
+# 从右边开始删除字符，删除最后一个'/'后结束
 echo ${dll%%/*}  # None
-#
+# 从右边开始删除字符，删除第一个'.'后结束
 echo ${dll%.*}   # /usr/local/lib/libpao.so
-#
+# 从右边开始删除字符，删除最后一个'.'后结束
 echo ${dll%%.*}  # /usr/local/lib/libpao
 
 echo $archive                # None
-#
+# 如果archive被定义，那么返回dll，但不改变archive的值
 echo ${archive:+$dll}        # None
-#
+# 如果archive为空或者被删除，那么返回dll，但不改变archive的值
 echo ${archive:-$dll}        # /usr/local/lib/libpao.so.1
-#
+# 如果archive为空或者被删除，那么返回dll，并将archive的值设置为dll
 echo ${archive:=$dll}        # /usr/local/lib/libpao.so.1
-#
+# 如果archive为空或者被删除，那么将消息送到标准错误输出
 echo ${archive:?'Failured!'} # /usr/local/lib/libpao.so.1
 
 archive='/usr/local/lib/libpao.a'
