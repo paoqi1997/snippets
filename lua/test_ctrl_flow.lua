67aef89af2ff9aa62a8dfe559f7f9ee3fd0f1793
paoqi1997 <604869221@qq.com>
2020-05-11 20:25:45 +0800

    lua: 分拆ctrl_flow代码段

---
+++ lua/test_ctrl_flow.lua
@@ -0,0 +1,35 @@
+-- Lua之控制流
+
+i, s = 0, ''
+while (i < 10) do
+    s = s .. i .. ' '
+    i = i + 1
+end
+print(s)
+
+s = ''
+for i = 1, 10, 3 do
+    s = s .. i .. ' '
+end
+print(s)
+
+pollers = {'iocp', 'select', 'poll', 'epoll'}
+for i, v in ipairs(pollers) do
+    print(string.format('pollers[%d]: %s', i, v))
+end
+
+i, s = 0, ''
+repeat
+    s = s .. i .. ' '
+    i = i + 1
+until (i >= 10)
+print(s)
+
+arch = 'amd64'
+if (arch == 'amd64') then
+    print('windows-x64')
+elseif (arch == '386') then
+    print('windows-x86')
+else
+    print('windows-arm')
+end
