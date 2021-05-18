eebc40385f6409e8310d015c901b36cc7184899b
paoqi1997 <604869221@qq.com>
2021-05-18 16:46:45 +0800

    python: copy

--- python/simple.py
+++ python/simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import copy
 import sys
 
 class Student:
@@ -25,6 +26,38 @@ def test_xxxals():
     print(f'locals: {locals()}')
     print(f'globals: {globals()}')
 
+def test_copy_dict():
+    dInfo = {
+        'name': 'mysql',
+        'version': '8.0.19'
+    }
+    print(dInfo)
+
+    dOutput = dInfo
+    dOutput['name'] = 'mariadb'
+    print(dInfo)
+
+    dOutput = dInfo.copy()
+    dOutput['name'] = 'mongodb'
+    print(dInfo)
+
+def test_deepcopy_dict():
+    dLang = {
+        'name': 'C++',
+        'os': [ 'windows', 'linux', 'macos' ],
+        'std': [ 'C++11', 'C++14', 'C++17', 'C++20' ]
+    }
+    print(dLang)
+
+    dOutput = copy.copy(dLang)
+    dOutput['name'] = 'Rust'
+    dOutput['std'].insert(0, 'C++98')
+    print(dLang)
+
+    dOutput = copy.deepcopy(dLang)
+    dOutput['os'].append('android')
+    print(dLang)
+
 if __name__ == '__main__':
     test_xxxals()
 
@@ -40,3 +73,6 @@ if __name__ == '__main__':
     stu2 = Student('kuikui')
     print(f'id: {stu2.m_ID}, name: {stu2.m_Name}')
     print(f'name_id: {Student.get_name_id(stu2)}')
+
+    test_copy_dict()
+    test_deepcopy_dict()
