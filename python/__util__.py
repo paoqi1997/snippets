d83a4abdc9b9985e69e78f1f86421c9601b0e72b
paoqi1997 <604869221@qq.com>
2021-07-22 11:01:25 +0800

    python: 增加说明

---
+++ python/__util__.py
@@ -0,0 +1,56 @@
+import copy
+
+class Student:
+    stuID = 0
+    def __init__(self, sName: str) -> None:
+        self.m_Name = sName
+        self.m_ID = self.__id__()
+
+    @classmethod
+    def __id__(cls) -> int:
+        cls.stuID += 1
+        return cls.stuID
+
+    @staticmethod
+    def get_name_id(stu) -> str:
+        return f'{stu.m_Name}_{stu.m_ID}'
+
+def test_xxxals():
+    lstn = list(range(10))
+    lstnewn = list(filter(lambda x: x % 2 != 0, lstn))
+    print(lstnewn)
+
+    print(f'locals: {locals()}')
+    print(f'globals: {globals()}')
+
+def test_copy_dict():
+    dInfo = {
+        'name': 'mysql',
+        'version': '8.0.19'
+    }
+    print(dInfo)
+
+    dOutput = dInfo
+    dOutput['name'] = 'mariadb'
+    print(dInfo)
+
+    dOutput = dInfo.copy()
+    dOutput['name'] = 'mongodb'
+    print(dInfo)
+
+def test_deepcopy_dict():
+    dLang = {
+        'name': 'C++',
+        'os': [ 'windows', 'linux', 'macos' ],
+        'std': [ 'C++11', 'C++14', 'C++17', 'C++20' ]
+    }
+    print(dLang)
+
+    dOutput = copy.copy(dLang)
+    dOutput['name'] = 'Rust'
+    dOutput['std'].insert(0, 'C++98')
+    print(dLang)
+
+    dOutput = copy.deepcopy(dLang)
+    dOutput['os'].append('android')
+    print(dLang)
--- python/simple.py
+++ python/simple.py
@@ -2,82 +2,43 @@
 
 from functools import partial
 
-import copy
 import sys
 
-class Student:
-    stuID = 0
-    def __init__(self, sName: str) -> None:
-        self.m_Name = sName
-        self.m_ID = self.__id__()
-
-    @classmethod
-    def __id__(cls) -> int:
-        cls.stuID += 1
-        return cls.stuID
-
-    @staticmethod
-    def get_name_id(stu) -> str:
-        return f'{stu.m_Name}_{stu.m_ID}'
-
-def test_xxxals():
-    lstn = list(range(10))
-    lstnewn = list(filter(lambda x: x % 2 != 0, lstn))
-    print(lstnewn)
-
-    print(f'locals: {locals()}')
-    print(f'globals: {globals()}')
-
-def test_copy_dict():
-    dInfo = {
-        'name': 'mysql',
-        'version': '8.0.19'
-    }
-    print(dInfo)
-
-    dOutput = dInfo
-    dOutput['name'] = 'mariadb'
-    print(dInfo)
-
-    dOutput = dInfo.copy()
-    dOutput['name'] = 'mongodb'
-    print(dInfo)
-
-def test_deepcopy_dict():
-    dLang = {
-        'name': 'C++',
-        'os': [ 'windows', 'linux', 'macos' ],
-        'std': [ 'C++11', 'C++14', 'C++17', 'C++20' ]
-    }
-    print(dLang)
-
-    dOutput = copy.copy(dLang)
-    dOutput['name'] = 'Rust'
-    dOutput['std'].insert(0, 'C++98')
-    print(dLang)
-
-    dOutput = copy.deepcopy(dLang)
-    dOutput['os'].append('android')
-    print(dLang)
+import __util__ as ut
 
 if __name__ == '__main__':
-    test_xxxals()
+    print('[py/(filter and others)]')
+
+    ut.test_xxxals()
+
+    print('[py/(hasattr and getattr)]')
 
     if hasattr(sys, 'platform'):
         print('sys.platform=%s'%getattr(sys, 'platform'))
 
+    print('[py/format]')
+
     print('{0} {2} {0}{1}'.format('Java', 'Script', '&'))
 
-    stu1 = Student('paoqi')
+    print('[py/(classmethod and staticmethod)]')
+
+    stu1 = ut.Student('paoqi')
     print(f'id: {stu1.m_ID}, name: {stu1.m_Name}')
-    print(f'name_id: {Student.get_name_id(stu1)}')
+    print(f'name_id: {ut.Student.get_name_id(stu1)}')
 
-    stu2 = Student('kuikui')
+    stu2 = ut.Student('kuikui')
     print(f'id: {stu2.m_ID}, name: {stu2.m_Name}')
-    print(f'name_id: {Student.get_name_id(stu2)}')
+    print(f'name_id: {ut.Student.get_name_id(stu2)}')
 
-    test_copy_dict()
-    test_deepcopy_dict()
+    print('[py/copy]')
+
+    ut.test_copy_dict()
+
+    print('[py/deepcopy]')
+
+    ut.test_deepcopy_dict()
+
+    print('[py/partial]')
 
     _pow2 = lambda base, exp: pow(base, exp)
     pow2 = partial(_pow2, exp=2)
