bacfd8b46d5bfbe3b0a8eb2f6609f4e379370a50
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2021-10-10 20:59:47 +0800

    js: test_moment

--- js/hello-nm/test_moment.js
+++ js/hello-nm/test_moment.js
@@ -22,14 +22,51 @@ function StrToInt13(s) {
 }
 
 /**
- * 计算从开始时间到今天是第几天
+ * 给定时间是否不早于当天指定时分秒的时间
+ * @param {number} timestamp 时间戳
+ * @param {string} Hms 时分秒
+ * @returns 比较结果
+ */
+function laterThan(timestamp, Hms) {
+    const YMD = m(timestamp).format(fmt2);
+    const ts = m(`${YMD} ${Hms}`, fmt).valueOf();
+    return timestamp >= ts;
+}
+
+/**
+ * 以每天零点为分界线，计算从开始时间到今天是第几天
  * @param {string} startTime 开始时间
  * @param {number} n 开始时间作为第 n 天
  * @returns 第几天
  */
 function dayN(startTime, n) {
-    n = n || 1;
+    n = n === undefined ? 1 : n;
     return m().diff(m(startTime, fmt2), 'days') + n;
+}
+
+/**
+ * 以每天指定时分秒为分界线，计算从开始时间到今天是第几天
+ * @param {number} startTime 开始时间
+ * @param {string} Hms 时分秒
+ * @param {number} n 开始时间作为第 n 天
+ * @returns 第几天
+ */
+function dayN_Hms(startTime, Hms, n) {
+    let ans = 0;
+
+    if (!laterThan(startTime, Hms)) {
+        ans += 1;
+    }
+
+    const YMD = m(startTime).format(fmt2);
+
+    ans += dayN(YMD, 0) + n;
+
+    if (!laterThan(Int13(), Hms)) {
+        ans -= 1;
+    }
+
+    return ans;
 }
 
 let v = Int10();
@@ -46,3 +83,9 @@ console.log(v);
 
 console.log(dayN('2021-10-01'));
 console.log(dayN('2021-10-01', 3));
+
+v = StrToInt13('2021-10-07 04:59:59');
+console.log(dayN_Hms(v, '05:00:00', 0));
+
+v = StrToInt13('2021-10-07 05:59:59');
+console.log(dayN_Hms(v, '05:00:00', 0));
