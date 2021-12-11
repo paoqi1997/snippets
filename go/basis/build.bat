5e4bb700569b2ba6565da4272760b33f29d7685f
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2021-12-11 17:22:59 +0800

    chore(go): [basis] build.bat检查ERRORLEVEL

--- go/basis/build.bat
+++ go/basis/build.bat
@@ -1,5 +1,18 @@
 :: build script for Windows
 @echo off
+
 set GOPATH=%cd%
 set GO111MODULE=off
+
 go build -o app.exe main
+
+if not %ERRORLEVEL% == 0 goto ExitError
+goto Done
+
+:ExitError
+exit /b 1
+
+:ExitOk
+exit /b 0
+
+:Done
