c3b66343e3eb7b82a1a05bb8c25e0d8306200394
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2021-12-13 19:48:47 +0800

    feat(csharp): add

---
+++ csharp/main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorldApplication
+{
+    // https://docs.microsoft.com/zh-cn/dotnet/csharp/tour-of-csharp/
+    class HelloWorld
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
