1a0ee103e5283bee9f82aa479f4e9c54e5f66f56
ldq <laidaoqi@earth.com>
2021-12-27 15:59:38 +0800

    fix(js): [hello-nm] update test_lodash.js

--- js/hello-nm/test_lodash.js
+++ js/hello-nm/test_lodash.js
@@ -10,4 +10,38 @@ function test_compact() {
     console.log(_.compact(li));
 }
 
-test_compact();
+function test_omit() {
+    console.log('TEST$_.omit');
+
+    const d = { bp: 1, pq: 2 };
+    console.log(d);
+    console.log(_.omit(d, 'bp'));
+}
+
+function test_random() {
+    console.log('TEST$_.random');
+
+    const val = _.random(10 ** 3, 10 ** 4 - 1, false);
+    console.log(val);
+}
+
+function test_uniqWith() {
+    console.log('TEST$_.uniqWith');
+
+    const l = [1, 1, 2, 2, 3];
+    console.log(l);
+    console.log(_.uniqWith(l, _.isEqual));
+
+    const li = [{ x: 1, y: 2 }, { x: 2, y: 2 }, { x: 2, y: 2 }, { x: 2, y: 3 }];
+    console.log(li);
+    console.log(_.uniqWith(li, _.isEqual));
+}
+
+function tests() {
+    test_compact();
+    test_omit();
+    test_random();
+    test_uniqWith();
+}
+
+tests();
