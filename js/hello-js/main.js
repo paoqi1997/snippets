a51359ea9f644288d6ea58c5621f1dac8fe84ca9
anonymity <somebody@earth.com>
2022-03-19 16:53:50 +0800

    refactor(js): [hello-js] functional

--- js/hello-js/main.js
+++ js/hello-js/main.js
@@ -2,124 +2,166 @@
 
 const util = require('./util');
 
-util.test_list();
-util.test_array();
+function test_sortMap() {
+    util.printUnit('sortMap');
 
-util.test_dict();
-util.test_map();
+    const mapobj = new Map([
+        ['redis', '5.0.7'],
+        ['mysql', '8.0.19'],
+        ['mariadb', '10.3.22']
+    ]);
+    console.log(mapobj);
 
-util.printUnit('sortMap');
+    util.sortMap(mapobj);
+    console.log(mapobj);
+}
 
-let mapobj = new Map([
-    ['redis', '5.0.7'],
-    ['mysql', '8.0.19'],
-    ['mariadb', '10.3.22']
-]);
-console.log(mapobj);
+function test_sortedMap() {
+    util.printUnit('sortedMap');
 
-util.sortMap(mapobj);
-console.log(mapobj);
+    let mymapobj = new Map([
+        [6, '3'], [4, '2'], [2, '1']
+    ]);
+    console.log(mymapobj);
 
-util.printUnit('sortedMap');
+    mymapobj = util.sortedMap(mymapobj);
+    console.log(mymapobj);
+}
 
-let mymapobj = new Map([
-    [6, '3'], [4, '2'], [2, '1']
-]);
-console.log(mymapobj);
+function test_eval() {
+    util.printUnit('eval');
 
-mymapobj = util.sortedMap(mymapobj);
-console.log(mymapobj);
+    const x = 2; const y = 4;
+    const z = eval(x + y);
+    console.log(`Result: ${z}`);
+}
 
-util.printUnit('eval');
+function test_Date() {
+    util.printUnit('Date');
 
-const x = 2; const y = 4;
-const z = eval(x + y);
-console.log(`Result: ${z}`);
+    console.log(util.getTimeString());
 
-util.printUnit('Date');
+    const sTime = '2038-03-08 20:00:00';
+    const oDate = new Date(Date.parse(sTime));
+    console.log(util.getTimeString(oDate));
+}
 
-console.log(util.getTimeString());
+function test_Promise() {
+    const unit1 = util.getUnit('Promise');
 
-const sTime = '2038-03-08 20:00:00';
-const oDate = new Date(Date.parse(sTime));
-console.log(util.getTimeString(oDate));
+    util.getPromise().then(value => {
+        console.log(`${unit1}\nValue_(1): ${value}`);
+    }).catch(reason => {
+        console.log(`${unit1}\nReason(1): ${reason}`);
+    });
+}
 
-const unit1 = util.getUnit('Promise');
+function test_AsyncAndAwait() {
+    const unit2 = util.getUnit('(async/await)');
 
-util.getPromise().then(value => {
-    console.log(`${unit1}\nValue_(1): ${value}`);
-}).catch(reason => {
-    console.log(`${unit1}\nReason(1): ${reason}`);
-});
+    util.F(unit2).catch(reason => console.log(`${unit2}\nReason(2): ${reason}`));
+}
 
-const unit2 = util.getUnit('(async/await)');
+function test_functionSTAR() {
+    util.printUnit('function*');
 
-util.F(unit2).catch(reason => console.log(`${unit2}\nReason(2): ${reason}`));
+    const g = util.gen(1);
+    console.log(`n1: ${g.next().value}`);  // 2
+    console.log(`n2: ${g.next(2).value}`); // 12
+    console.log(`n3: ${g.next(3).value}`); // 8
+    console.log(`n4: ${g.next(4).value}`); // undefined
+}
 
-util.printUnit('function*');
+function test_closure() {
+    util.printUnit('closure');
 
-const g = util.gen(1);
-console.log(`n1: ${g.next().value}`);  // 2
-console.log(`n2: ${g.next(2).value}`); // 12
-console.log(`n3: ${g.next(3).value}`); // 8
-console.log(`n4: ${g.next(4).value}`); // undefined
+    const sumFunc = util.sum();
+    console.log(sumFunc(1));
+    console.log(sumFunc(2));
+    console.log(sumFunc(3));
+    console.log(sumFunc(4));
+}
 
-util.printUnit('closure');
+function test_currying() {
+    util.printUnit('currying');
 
-const sumFunc = util.sum();
-console.log(sumFunc(1));
-console.log(sumFunc(2));
-console.log(sumFunc(3));
-console.log(sumFunc(4));
+    const isNumber = util.curryingCheck(/[0-9]+/);
+    const isLetter = util.curryingCheck(/[a-zA-Z]+/);
 
-util.printUnit('currying');
+    console.log(isNumber(1));
+    console.log(isNumber('a'));
+    console.log(isLetter(1));
+    console.log(isLetter('a'));
+}
 
-const isNumber = util.curryingCheck(/[0-9]+/);
-const isLetter = util.curryingCheck(/[a-zA-Z]+/);
+function test_merge_delWith_deepcopy() {
+    util.printUnit('merge');
 
-console.log(isNumber(1));
-console.log(isNumber('a'));
-console.log(isLetter(1));
-console.log(isLetter('a'));
+    const d1 = {
+        1: 2, 2: 4, 3: 6, 'd': {'k1': 'v1', 'k2': 'v2', 'k3': [1, 3, 5, 7, 9]},
+        'l': [{'k_': 'v_'}]
+    };
+    const d2 = { 1: 3, 2: 5, 'l': [{'kk': 'vv'}] };
 
-util.printUnit('merge');
+    console.log(d2);
+    util.merge(d1, d2);
+    console.log(d2);
 
-const d1 = {
-    1: 2, 2: 4, 3: 6, 'd': {'k1': 'v1', 'k2': 'v2', 'k3': [1, 3, 5, 7, 9]},
-    'l': [{'k_': 'v_'}]
-};
-const d2 = { 1: 3, 2: 5, 'l': [{'kk': 'vv'}] };
+    const l1 = { 'list': [ ['v_h', 'v_s'], ['v_n'], ['v_g', 'v_g'] ] };
+    const l2 = { 'list': [ ['v_r', 'v_s', 'v_c'], ['v_m', 'v_m'] ] };
 
-console.log(d2);
-util.merge(d1, d2);
-console.log(d2);
+    console.log(l2);
+    util.merge(l1, l2);
+    console.log(l2);
 
-const l1 = { 'list': [ ['v_h', 'v_s'], ['v_n'], ['v_g', 'v_g'] ] };
-const l2 = { 'list': [ ['v_r', 'v_s', 'v_c'], ['v_m', 'v_m'] ] };
+    util.printUnit('delWith');
 
-console.log(l2);
-util.merge(l1, l2);
-console.log(l2);
+    console.log(d2);
+    util.delWith(d2, { 'd': { 'k1': '?' } });
+    console.log(d2);
 
-util.printUnit('delWith');
+    util.printUnit('deepcopy');
 
-console.log(d2);
-util.delWith(d2, { 'd': { 'k1': '?' } });
-console.log(d2);
+    const d3 = util.deepcopy(d1);
 
-util.printUnit('is_type');
+    d3[1] = '?';
+    console.log(d1);
+    console.log(d3);
+}
 
-console.log(util.is_type('', 'string'));
-console.log(util.is_type(11, 'number'));
-console.log(util.is_type([], 'array'));
-console.log(util.is_type({}, 'object'));
+function test_isType() {
+    util.printUnit('is_type');
 
-util.printUnit('deepcopy');
+    console.log(util.is_type('', 'string'));
+    console.log(util.is_type(11, 'number'));
+    console.log(util.is_type([], 'array'));
+    console.log(util.is_type({}, 'object'));
+}
 
-const d3 = util.deepcopy(d1);
+function tests() {
+    util.test_list();
+    util.test_array();
 
-d3[1] = '?';
-console.log(d1);
-console.log(d3);
+    util.test_dict();
+    util.test_map();
 
-util.test_exception();
+    test_sortMap();
+    test_sortedMap();
+
+    test_eval();
+    test_Date();
+
+    test_Promise();
+    test_AsyncAndAwait();
+    test_functionSTAR();
+
+    test_closure();
+    test_currying();
+
+    test_merge_delWith_deepcopy();
+    test_isType();
+
+    util.test_exception();
+}
+
+tests();
