23535798579305ce498c5fa83890775936607117
anonymity <somebody@earth.com>
2023-03-11 22:28:53 +0800

    fix(js): [hello-nm.test_lodash] reduce

--- js/hello-nm/test_lodash.js
+++ js/hello-nm/test_lodash.js
@@ -5,6 +5,23 @@
  */
 const _ = require('lodash');
 
+function tests() {
+    test_compact();
+    test_get_set();
+    test_assign_omit_pick();
+    test_random();
+    test_uniqWith();
+    test_concat();
+    test_remove();
+    test_filter();
+    test_keyBy();
+    test_reduce();
+    test_findIndex();
+    test_sortBy();
+}
+
+tests();
+
 function test_compact() {
     console.log('TEST$_.compact');
 
@@ -124,6 +141,40 @@ function test_keyBy() {
     console.log(list); // { '1': { id: 1 }, '2': { id: 2 }, '3': { id: 3 } }
 }
 
+function test_reduce() {
+    console.log('TEST$_.reduce');
+
+    const v1weights = [
+        { id: 1, weight: 100 },
+        { id: 2, weight: 300 },
+        { id: 3, weight: 600 }
+    ];
+
+    const v2weights = [
+        { idx: 1, id: 1, weight: 10 },
+        { idx: 2, id: 1, weight: 20 },
+        { idx: 3, id: 1, weight: 30 },
+        { idx: 4, id: 1, weight: 50 },
+        { idx: 5, id: 2, weight: 100 },
+        { idx: 6, id: 2, weight: 300 },
+        { idx: 7, id: 2, weight: 600 },
+        { idx: 8, id: 2, weight: 800 },
+        { idx: 9, id: 3, weight: 1200 },
+        { idx: 10, id: 3, weight: 3000 }
+    ];
+
+    const pool = {
+        '1': 0, '2': 1, '3': 1, '4': 1, '5': 1,
+        '6': 0, '7': 1, '8': 0, '9': 1, '10': 0
+    };
+
+    const ww = calNewWights(v1weights, v2weights, pool);
+    const weights = _.values(ww);
+
+    const rolledWeight = rollWeight(weights);
+    console.log(rolledWeight);
+}
+
 function test_findIndex() {
     console.log('TEST$_.findIndex');
 
@@ -160,18 +211,90 @@ function test_sortBy() {
     console.log(li, ll);
 }
 
-function tests() {
-    test_compact();
-    test_get_set();
-    test_assign_omit_pick();
-    test_random();
-    test_uniqWith();
-    test_concat();
-    test_remove();
-    test_filter();
-    test_keyBy();
-    test_findIndex();
-    test_sortBy();
+function calNewWights(v1weights, v2weights, pool) {
+    const totalWeight = _.reduce(v1weights, (accum, x) => accum + x.weight, 0);
+    console.log(`totalWeight: ${totalWeight}`);
+
+    const v1divider = {};
+    const id2count = {};
+
+    for (const v1weight of v1weights) {
+        v1divider[v1weight.id] = v1weight.weight / totalWeight;
+        id2count[v1weight.id] = 0;
+    }
+
+    console.log(`v1divider: ${JSON.stringify(v1divider)}`);
+
+    const idx2id = {};
+    const oldIdx2Weight = {};
+    const idx2weight = {};
+
+    for (const v2weight of v2weights) {
+        idx2id[v2weight.idx] = v2weight.id;
+        oldIdx2Weight[v2weight.idx] = v2weight.weight;
+        idx2weight[v2weight.idx] = v2weight.weight;
+    }
+
+    for (const idx in pool) {
+        const id = idx2id[idx];
+        if (pool[idx] > 0) {
+            id2count[id] += 1;
+        }
+    }
+
+    console.log(`id2count: ${JSON.stringify(id2count)}`);
+
+    for (const idx in pool) {
+        if (pool[idx] > 0) {
+            continue;
+        }
+
+        const weight = oldIdx2Weight[idx];
+        idx2weight[idx] = 0;
+
+        const dividedV1Weights = {};
+
+        for (const id in v1divider) {
+            dividedV1Weights[id] = weight * v1divider[id];
+        }
+
+        console.log(`dividedV1Weights<[${idx}]{${weight}}>: ${JSON.stringify(dividedV1Weights)}`);
+
+        const dividedV2Weights = {};
+
+        for (const id in id2count) {
+            dividedV2Weights[id] = dividedV1Weights[id] / id2count[id];
+        }
+
+        console.log(`dividedV2Weights<[${idx}]{${weight}}>: ${JSON.stringify(dividedV2Weights)}`);
+
+        for (const index in idx2weight) {
+            if (idx2weight[index] <= 0) {
+                continue;
+            }
+
+            idx2weight[index] += dividedV2Weights[idx2id[index]];
+        }
+
+        console.log(`idx2weight<[${idx}]{${weight}}>: ${JSON.stringify(idx2weight)}`);
+    }
+
+    return idx2weight;
 }
 
-tests();
+function rollWeight(weights) {
+    const totalWeight = weights.reduce((accum, curr) => accum + curr);
+    let target = Math.floor(Math.random() * totalWeight) + 1;
+
+    console.log(`totalWeight: ${totalWeight}, target: ${target}`);
+
+    for (let i = 0; i < weights.length; i += 1) {
+      if (weights[i] > 0 && target <= weights[i]) {
+        return { index: i, weight: weights[i] };
+      }
+
+      target -= weights[i];
+    }
+
+    return { index: 0, weight: -1 };
+}
