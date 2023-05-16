c2aa1b154a2574c1068c08a248d4ce7dbdbe3215
anonymity <somebody@earth.com>
2023-05-16 17:45:41 +0800

    fix(ts): [hello-ts] enum

--- ts/hello-ts/main.ts
+++ ts/hello-ts/main.ts
@@ -2,6 +2,7 @@ import * as util from './util';
 
 function tests() {
   test_class();
+  test_enum();
 }
 
 tests();
@@ -16,4 +17,33 @@ function test_class() {
 
   console.log(stu.getGrade('medicine'));
   console.log(stu.getGrade('chemistry'));
+}
+
+function test_enum() {
+  console.log('TEST$_.enum');
+
+  enum LOG_LEVEL {
+    DEBUG = 1,
+    INFO,
+    WARN,
+  };
+
+  const level = LOG_LEVEL['INFO'];
+
+  switch (<any> level as any) {
+    case LOG_LEVEL.DEBUG:
+      console.log('pass DEBUG');
+    case LOG_LEVEL.INFO: {
+      console.log('pass INFO');
+    }
+    case LOG_LEVEL.WARN:
+      console.log('pass WARN');
+      break;
+    default:
+      console.log('END');
+  }
+
+  if ([LOG_LEVEL.INFO, LOG_LEVEL.WARN].includes(level)) {
+    console.log('YES!');
+  }
 }
