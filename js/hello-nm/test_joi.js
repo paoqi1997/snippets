8dfb2e05258a985b0f6611625188e4cff854ca9c
anonymity <somebody@earth.com>
2023-10-08 17:26:47 +0800

    fix(js): [hello-nm] update test_joi

--- js/hello-nm/test_joi.js
+++ js/hello-nm/test_joi.js
@@ -11,12 +11,23 @@ function tests() {
 
 tests();
 
+function print(msg) {
+    console.log(`>>> TEST$${msg}`);
+}
+
 function test() {
     const schema = joi.object({
         skipReducing: joi.number().optional(),
+        clanCount: joi.number().integer().min(0).optional(),
     });
 
     console.log(schema.validate({}));
     console.log(schema.validate({ skipReducing: 1 }));
     console.log(schema.validate({ name: 'paoqi' }));
+
+    print('clanCount');
+
+    console.log(schema.validate({ clanCount: null }));
+    console.log(schema.validate({ clanCount: -1 }));
+    console.log(schema.validate({ clanCount: 1 }));
 }
