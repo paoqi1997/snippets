1469948418290c662fd419d682cc17c83329eaab
anonymity <somebody@earth.com>
2023-10-16 23:06:13 +0800

    fix(go): [basis] fix

--- go/basis/src/main/tests.go
+++ go/basis/src/main/tests.go
@@ -34,7 +34,7 @@ func TEST_slice() {
     slice1, slice2 := m[:3], m[3:]
     fmt.Println(len(slice1), cap(slice1), slice1)
     fmt.Println(len(slice2), cap(slice2), slice2)
- 
+
     slice := make([]int, 3, 4)
     fmt.Println(len(slice), cap(slice), slice)
 
