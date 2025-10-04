f628a9a72154e85dbd71298aab31ad57c5c40c66
paoqi1997 <604869221@qq.com>
2025-10-04 20:23:04 +0800

    fix(csharp): update playground

--- csharp/playground/Program.cs
+++ csharp/playground/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MemoryPack;
+
+namespace Playground
+{
+    [MemoryPackable]
+    public partial class Any
+    {
+        [MemoryPackOrder(0)]
+        public int IntValue { get; set; } = 0;
+        [MemoryPackOrder(1)]
+        public float FloatValue { get; set; } = 0f;
+        [MemoryPackOrder(2)]
+        public string? StringValue { get; set; } = null;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var anyObj = new Any{
+                IntValue = 123,
+                FloatValue = 123.456f,
+                StringValue = "123",
+            };
+
+            byte[] bytes = MemoryPackSerializer.Serialize(anyObj);
+
+            Any? decAnyObj = MemoryPackSerializer.Deserialize<Any>(bytes);
+
+            if (decAnyObj != null) {
+                Console.WriteLine(decAnyObj.FloatValue);
+            }
+        }
+    }
+}
