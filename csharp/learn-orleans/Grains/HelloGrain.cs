b30d6ab114456fc6cee0e46aa7f52e7bd5d1b00a
paoqi1997 <604869221@qq.com>
2025-10-05 03:08:19 +0800

    fix(csharp): [learn-orleans] update Grains

--- csharp/learn-orleans/Grains/HelloGrain.cs
+++ csharp/learn-orleans/Grains/HelloGrain.cs
@@ -1,6 +1,24 @@
-﻿namespace Grains;
+﻿using Microsoft.Extensions.Logging;
+using GrainInterfaces;
 
-public class Class1
+namespace Grains;
+
+public class HelloGrain : Grain, IHello
 {
+    private readonly ILogger _logger;
 
+    public HelloGrain(ILogger<HelloGrain> logger) => _logger = logger;
+
+    ValueTask<string> IHello.SayHello(string greeting)
+    {
+        _logger.LogInformation("""
+            SayHello message received: greeting = "{Greeting}"
+            """,
+            greeting);
+        
+        return ValueTask.FromResult($"""
+
+            Client said: "{greeting}", so HelloGrain says: Hello!
+            """);
+    }
 }
